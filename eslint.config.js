96d71c2fcb9da06bd616b71c5b5d97d62550c888
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-01-18 12:35:53 +0530

    chore: lint jsdoc comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,19 @@
 import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import jsdoc from "eslint-plugin-jsdoc";
+import globals from "globals";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: {
+      jsdoc,
+    },
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+  },
 ]);
