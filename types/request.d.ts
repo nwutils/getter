da83de8a00eb155fd352b8ca9f6c4a66649823e9
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2026-01-18 12:54:40 +0530

    chore: convert JSDoc comments to TypeScript types

---
+++ types/request.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Download from `url` and save at `filePath`.
+ * @param {string} url
+ * @param {string} filePath
+ * @returns {Promise<void>}
+ */
+export default function request(url: string, filePath: string): Promise<void>;
